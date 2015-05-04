ec3f7635078d1fe5f6750d1b46efa69afcc9fa7c
puapao <puapao123@hotmail.co.uk>
2015-05-04 17:54:16 +1000

    update

--- Our Work/Game Files/testGame.c
+++ Our Work/Game Files/testGame.c
@@ -5,6 +5,32 @@
 // 1/05/2015
 // This program communicates with game.h to play Knowledge Island.
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "game.h"
+
+
 int main(int argc, char *argv[]) {
+   action discFrom;
+   discFrom = 3;
+   //if discFrom = 3
+   //then discTo = 1
+   //but discFrom >= 3
+   action discTo;
+   discTo = discFrom/3;
+   assert (discTo == 1);
+   
+   //this is for actioncode in action struct
+   //this is a intend
+   action actCode;
+   actCode = PASS;
+   action actExecute;
+   actExecute = actCode;
+   assert (actExecute == 0);
+
+   printf ("You've passed all test!");
+   printf ("You're awesome!");
 	// MAIN STUFF, ADD THIS WHEN THE PLAN IS DONE.
 }
